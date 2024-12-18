52b7bce0df9d25de00ca3d4d6bf6f553c20b4930
huzaifa-abajwa <167539477+huzaifa-abajwa@users.noreply.github.com>
2024-12-18 23:59:11 +0500

    Update sample_script.sh

--- sample_script.sh
+++ sample_script.sh
@@ -1,1 +1,1 @@
-sshpass -p ' ' ssh HP@192.168.18.8 'cmd /c "echo log.txt > D:\Academics\LLM\Research\output.txt"' && ollama run llama3.2:1b-instruct-q2_K "Explain the water cycle in simple terms" --verbose && sshpass -p ' ' ssh HP@192.168.18.8 'cmd /c "echo exit > D:\Academics\LLM\Research\output.txt"'
+sshpass -p password ssh username@ip 'cmd /c "echo log.txt > signal.txt' && ollama run llama3.2:1b-instruct-q2_K "Explain the water cycle in simple terms" --verbose && sshpass -p password ssh username@ip 'cmd /c "echo exit > signal.txt"'
